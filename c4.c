26dda31bfd27abcb65c1bf30b069468b3f1d3b0a
noname <amalaric@gill-industries.com>
2017-03-01 15:03:02 -0500

    Added simple print statements. Filtered input for players pieces X/O. Added a win condition and error checking for that. Added class turn to keep track of the players turn and return a print statement, also asks and passes the position to the placePiece function. Questions? Ask! This is still a work in progress.

--- c4.c
+++ c4.c
@@ -1,26 +1,133 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <ctype.h>
 //#include "file_utils.h"
-#define WIDTH 7 
+#define WIDTH 7
 #define HEIGHT 6
 
-void drawBoard(int width, int height){
+//custom functions//
+void Board(int width, int height);
+void placePiece(int position, int turn_num);
+void turn(int turn_num);
 
+
+
+int main(int argc, char **argv) {
+    // if(arc ==
+    
+    //take in user input here//
+    //start with the piece type, X or O//
+    char player_one_piece, player_two_piece;
+    int win_condit;
+    
+    
+    printf("--------------------------------------------------------------------\n");
+    printf("Enter either X or O for player 1: ");
+    scanf("%c", &player_one_piece);
+    player_one_piece = toupper(player_one_piece);
+    
+    
+    if(player_one_piece == 'X' ){
+        player_two_piece = 'O';
+        
+        printf("Player 1: %c\n", player_one_piece);
+        printf("Player 2: %c\n", player_two_piece);
+        printf("--------------------------------------------------------------------\n");
+        
+        
+    }else if(player_one_piece == 'O' ){
+        player_two_piece = 'X';
+        
+        printf("Player 1: %c\n", player_one_piece);
+        printf("Player 2: %c\n", player_two_piece);
+        printf("--------------------------------------------------------------------\n");
+        
+        
+    }else{
+        printf("\nInvalid input!\n Assigned player 1 to X, player 2 to O!\n");
+        printf("Board is generating...\n");
+        
+        player_one_piece = 'X';
+        player_two_piece = 'O';
+        
+        printf("Player 1: %c\n", player_one_piece);
+        printf("Player 2: %c\n", player_two_piece);
+        printf("--------------------------------------------------------------------\n");
+        
+    }
+    printf("--------------------------------------------------------------------\n");
+    printf("Enter your win condition(#):");
+    scanf("%d", &win_condit);
+    
+    
+    if(isdigit(win_condit) || win_condit <= 0){
+        printf("Incorrect input!\n");
+        printf("Assigning default win condition of 4! Happy playing!\n");
+        printf("--------------------------------------------------------------------\n");
+        
+        win_condit = 4;
+    }
+    
+    printf("It will take %d touching pieces of the same type to win! Good Luck!\n", win_condit);
+    printf("--------------------------------------------------------------------\n");
+    
+    
+    Board(WIDTH, HEIGHT);
+    int turn_num = 0;
+    
+    turn(turn_num);
+    
+    
+    
+    //call placePiece here//
+    
+    
+}
+
+/**
+ * Initilizes the board
+ * @param width width of the board
+ * @param height height of the board
+ */
+void Board(int width, int height){
+    
     char grid[WIDTH][HEIGHT];
     for(int i = 0; i < height; i++){
         for(int j = 0; j < width; j++){
-           grid[i][j] = '*';
+            grid[i][j] = '*';
             printf("%c ", grid[i][j]);
         }
         printf("\n");
     }
+    
 }
 
-
-int main(int argc, char **argv) {
-   // if(arc == 
-   drawBoard(WIDTH, HEIGHT);
+void placePiece(int position, int turn){
+    printf("here\n");
+    turn++;
+    
+    
+    
 }
-
+/**
+ *Keep track and print out the current players turn.
+ *@param turn_num turn for the current player
+ */
+void turn(int turn_num){
+    
+    int position_two, position_one;
+    
+    if(turn_num == 0){
+        printf("Player 1! Your turn!\n");
+        printf("Enter position to place piece: ");
+        scanf("%d", &position_one);
+        placePiece(position_one, turn_num);
+    }else if (turn_num % 2 == 0){
+        printf("Player 2! Your turn!\n");
+        printf("Enter position to place piece: ");
+        scanf("%d", &position_two);
+        placePiece(position_two, turn_num);
+    }
+}
 
